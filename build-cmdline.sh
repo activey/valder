d52c2db1ef2fc6b0828f60c5347b103bf12b95bd
Łukasz Grabski <activey@doaplatform.org>
2015-11-22 20:12:00 +0100

    #42 - fixed some differencies between build scripts, fixed library path problem for valder runtime

--- build-cmdline.sh
+++ build-cmdline.sh
@@ -2,7 +2,7 @@
 set -x
 
 SOURCES=(`find src/main -type f -name "**.vala"`)
-PACKAGES=('glib-2.0' 'gobject-2.0' 'gio-2.0' 'json-glib-1.0' 'libvala-0.28' 'gmodule-2.0' 'gee-0.8' 'bob-0.0.1')
+PACKAGES=('glib-2.0' 'gobject-2.0' 'gio-2.0' 'json-glib-1.0' 'libvala-0.30' 'gmodule-2.0' 'gee-0.8' 'bob-0.0.1')
 VAPI_DIRS=('src/library/vapi')
 
 SOURCES_PREFIXED=$(printf "%s " "${SOURCES[@]}")
@@ -10,4 +10,4 @@ PACKAGES_PREFIXED=$(printf " --pkg %s" "${PACKAGES[@]}")
 VAPI_DIRS_PREFIXED=$(printf " --vapidir %s" "${VAPI_DIRS[@]}")
 
 mkdir -p target/plugins
-valac -g -v $SOURCES_PREFIXED -X -Wl,-rpath=\$ORIGIN/lib -X -Ltarget/lib -X -lbob -X -Isrc/library/c $VAPI_DIRS_PREFIXED $PACKAGES_PREFIXED -o target/bin/bob
+valac -g -v $SOURCES_PREFIXED -X -Wl,-rpath=\$ORIGIN/../lib -X -Ltarget/lib -X -lbob -X -Isrc/library/c $VAPI_DIRS_PREFIXED $PACKAGES_PREFIXED -o target/bin/bob
